c0c2b1e40883dfa50e7951049ce4d168795929a8
Jens Fischer <Jensfz@gmail.com>
2020-04-14 21:23:40 +0200

    added ETA

--- app/routes/overlay.js
+++ app/routes/overlay.js
@@ -1,14 +1,16 @@
-'use strict';
-const express = require('express');
+"use strict";
+const express = require("express");
 const router = express.Router();
 
-
-router.get('/progress', (req, res, next) => {
+router.get("/progress", (req, res, next) => {
 	res.render("progress");
 });
 
+router.get("/ETA", (req, res, next) => {
+	res.render("ETA");
+});
 
-router.get('/info', (req, res, next) => {
+router.get("/info", (req, res, next) => {
 	res.render("info");
 });
 
